df89f10bd10b472e5ecfa45b662891f69fcb3ee4
DJeovannyZ <darwinzaldana@gmail.com>
2025-04-30 00:33:09 -0600

    ZA20003: Creacionn de metodo calcular, consumo de servicio SOAP

--- app/Http/Controllers/CalculadoraController.php
+++ app/Http/Controllers/CalculadoraController.php
@@ -3,8 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use SoapClient;
 
 class CalculadoraController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('frontend.calculadora');
+    }
+
+    public function calcular(Request $request)
+    {
+        $numero1 = $request->input('num1');
+        $numero2 = $request->input('num2');
+        $operacion = $request->input('operacion');
+
+        $client = new \SoapClient("http://www.dneonline.com/calculator.asmx?WSDL");
+
+        try {
+            $params = ['intA' => $numero1, 'intB' => $numero2];
+            $resultado = null;
+
+            if ($operacion === 'sumar') {
+                $response = $client->Add($params);
+                $resultado = $response->AddResult;
+            } elseif ($operacion === 'multiplicar') {
+                $response = $client->Multiply($params);
+                $resultado = $response->MultiplyResult;
+            }
+            return view('frontend.calculadora', compact('resultado', 'numero1', 'numero2', 'operacion'));
+
+        } catch (\Exception $e) {
+            return back()->with('error', 'Error al consumir el servicio SOAP' . $e->getMessage());
+        }
+    }
 }
