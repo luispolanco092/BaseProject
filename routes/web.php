cfee0ea981797c6ed760fd7624fbe6e3cffbce93
DJeovannyZ <darwinzaldana@gmail.com>
2025-06-15 21:42:44 -0600

    creacion de modelo controlador y migracion de libro, se agrego ruta libro

--- routes/web.php
+++ routes/web.php
@@ -12,21 +12,22 @@ use App\Http\Controllers\XmlController;
 use App\Http\Controllers\CalculadoraController;
 
 use App\Http\Controllers\Backend\Dashboard\DashboardController;
+use App\Http\Controllers\LibroController;
 
-Route::get('/preferencias', [XmlController::class,'index']);
-Route::get('/preferencias', [XmlController::class,'mostrar']);
+Route::get('/preferencias', [XmlController::class, 'index']);
+Route::get('/preferencias', [XmlController::class, 'mostrar']);
 
 
 // --- LOGIN ---
 
-Route::get('/', [LoginController::class,'index'])->name('login');
+Route::get('/', [LoginController::class, 'index'])->name('login');
 
 Route::post('/admin/login', [LoginController::class, 'login']);
 Route::post('/admin/logout', [LoginController::class, 'logout'])->name('admin.logout');
 
 // --- CONTROL WEB ---
 
-Route::get('/panel', [ControlController::class,'indexRedireccionamiento'])->name('admin.panel');
+Route::get('/panel', [ControlController::class, 'indexRedireccionamiento'])->name('admin.panel');
 
 // --- CALCULADORA SOAP ---
 
@@ -34,20 +35,20 @@ Route::get('/calculadora', [CalculadoraController::class, 'index']);
 Route::post('/calcular', [CalculadoraController::class, 'calcular'])->name('calcular');
 // --- ROLES ---
 
-Route::get('/admin/roles/index', [RolesController::class,'index'])->name('admin.roles.index');
-Route::get('/admin/roles/tabla', [RolesController::class,'tablaRoles']);
-Route::get('/admin/roles/lista/permisos/{id}', [RolesController::class,'vistaPermisos']);
-Route::get('/admin/roles/permisos/tabla/{id}', [RolesController::class,'tablaRolesPermisos']);
+Route::get('/admin/roles/index', [RolesController::class, 'index'])->name('admin.roles.index');
+Route::get('/admin/roles/tabla', [RolesController::class, 'tablaRoles']);
+Route::get('/admin/roles/lista/permisos/{id}', [RolesController::class, 'vistaPermisos']);
+Route::get('/admin/roles/permisos/tabla/{id}', [RolesController::class, 'tablaRolesPermisos']);
 Route::post('/admin/roles/permiso/borrar', [RolesController::class, 'borrarPermiso']);
 Route::post('/admin/roles/permiso/agregar', [RolesController::class, 'agregarPermiso']);
-Route::get('/admin/roles/permisos/lista', [RolesController::class,'listaTodosPermisos']);
-Route::get('/admin/roles/permisos-todos/tabla', [RolesController::class,'tablaTodosPermisos']);
+Route::get('/admin/roles/permisos/lista', [RolesController::class, 'listaTodosPermisos']);
+Route::get('/admin/roles/permisos-todos/tabla', [RolesController::class, 'tablaTodosPermisos']);
 Route::post('/admin/roles/borrar-global', [RolesController::class, 'borrarRolGlobal']);
 
 // --- PERMISOS A USUARIOS ---
 
-Route::get('/admin/permisos/index', [PermisoController::class,'index'])->name('admin.permisos.index');
-Route::get('/admin/permisos/tabla', [PermisoController::class,'tablaUsuarios']);
+Route::get('/admin/permisos/index', [PermisoController::class, 'index'])->name('admin.permisos.index');
+Route::get('/admin/permisos/tabla', [PermisoController::class, 'tablaUsuarios']);
 Route::post('/admin/permisos/nuevo-usuario', [PermisoController::class, 'nuevoUsuario']);
 Route::post('/admin/permisos/info-usuario', [PermisoController::class, 'infoUsuario']);
 Route::post('/admin/permisos/editar-usuario', [PermisoController::class, 'editarUsuario']);
@@ -56,13 +57,14 @@ Route::post('/admin/permisos/extra-nuevo', [PermisoController::class, 'nuevoPerm
 Route::post('/admin/permisos/extra-borrar', [PermisoController::class, 'borrarPermisoGlobal']);
 
 // --- PERFIL DE USUARIO ---
-Route::get('/admin/editar-perfil/index', [PerfilController::class,'indexEditarPerfil'])->name('admin.perfil');
+Route::get('/admin/editar-perfil/index', [PerfilController::class, 'indexEditarPerfil'])->name('admin.perfil');
 Route::post('/admin/editar-perfil/actualizar', [PerfilController::class, 'editarUsuario']);
 
 // --- SIN PERMISOS VISTA 403 ---
-Route::get('sin-permisos', [ControlController::class,'indexSinPermiso'])->name('no.permisos.index');
+Route::get('sin-permisos', [ControlController::class, 'indexSinPermiso'])->name('no.permisos.index');
 
-Route::get('/admin/dashboard', [DashboardController::class,'vistaDashboard'])->name('admin.dashboard.index');
+Route::get('/admin/dashboard', [DashboardController::class, 'vistaDashboard'])->name('admin.dashboard.index');
 
 Route::get('/xml', [XmlController::class, 'mostrar']);
 
+Route::resource('libros', LibroController::class)->middleware('auth');
